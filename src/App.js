433e76c595f355a8a71747286bc10a1ec8baebc1
Docphils <docphils64@gmail.com>
2023-11-02 21:04:20 +0100

    First Component with tailwind css intalled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import "./App.css";
-
+import "./index.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
 function App() {
+  const [role, setRole] = useState("dev");
+  const showEmployee = true;
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="bg-blue-300">
+      {showEmployee ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              setRole(e.target.value);
+            }}
+          />
+
+          <Employee name="Philip" role="Teacher" />
+          <Employee name="Mercy" role={role} />
+          <Employee name="Jane" />
+          <Employee name="Grace" />
+        </>
+      ) : (
+        <p>You can't view employees</p>
+      )}
     </div>
   );
 }

79b5a9219154cba9508b8b395440489e962eda67
Docphils <docphils64@gmail.com>
2023-11-03 10:32:24 +0100

    Improved with images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
+
 function App() {
   const [role, setRole] = useState("dev");
   const showEmployee = true;
   return (
-    <div className="bg-blue-300">
+    <div className="m-2">
       {showEmployee ? (
         <>
           <input
@@ -14,11 +15,24 @@ function App() {
               setRole(e.target.value);
             }}
           />
-
-          <Employee name="Philip" role="Teacher" />
-          <Employee name="Mercy" role={role} />
-          <Employee name="Jane" />
-          <Employee name="Grace" />
+          <div className="flex flex-wrap">
+            <Employee
+              name="Philip"
+              role="Teacher"
+              img="/images/IMG-20230623-WA0009.jpg"
+            />
+            <Employee name="Mercy" role={role} img="/mercy.jpg" />
+            <Employee name="Jane" img="/jane.jpg" />
+            <Employee name="Grace" img="/forgrace.jpg" />
+            <Employee
+              name="Philip"
+              role="Teacher"
+              img="/images/IMG-20230623-WA0009.jpg"
+            />
+            <Employee name="Mercy" role={role} img="/mercy.jpg" />
+            <Employee name="Jane" img="/jane.jpg" />
+            <Employee name="Grace" img="/forgrace.jpg" />
+          </div>
         </>
       ) : (
         <p>You can't view employees</p>
